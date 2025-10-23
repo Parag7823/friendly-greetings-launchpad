13cec6e2d3b736d8d96ca3e48278303b85030add
Parag Pakhrani <Paragpakhrani@gmail.com>
2025-10-23 07:35:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start ARQ worker in background
-arq arq_worker.WorkerSettings &
+python -m arq arq_worker.WorkerSettings &
 
 # Start FastAPI server in foreground
 uvicorn fastapi_backend:app --host 0.0.0.0 --port $PORT
