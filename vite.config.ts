a139ac567a0b91805353390e52c26443897a62e7
Parag Pakhrani <Paragpakhrani@gmail.com>
2025-11-02 16:25:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
