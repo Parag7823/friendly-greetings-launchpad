8e47410e2b0ebf5e679ce6b7426f6519c51a48f7
Parag Pakhrani <Paragpakhrani@gmail.com>
2025-12-03 05:12:40 +0530

    Update ThreePanelLayout.tsx

--- src/components/ThreePanelLayout.tsx
+++ src/components/ThreePanelLayout.tsx
@@ -54,16 +54,16 @@ export const ThreePanelLayout = ({ currentView = 'chat', onNavigate }: ThreePane
     <div className="h-full w-full finley-dynamic-bg flex flex-col">
       {/* AUDIT FIX #5: Removed wasteful 48px header bar - Database button moved to TabbedFilePreview */}
       <PanelGroup direction="horizontal" className="h-full flex-1">
-        {/* Chat Panel - 60% default */}
-        <Panel defaultSize={60} minSize={40} maxSize={80} className="relative">
+        {/* Chat Panel - 40% default */}
+        <Panel defaultSize={40} minSize={20} maxSize={60} className="relative">
           <ChatInterface currentView={currentView} onNavigate={onNavigate} />
         </Panel>
 
         {/* Resize Handle - Single copper line */}
         <PanelResizeHandle className="w-0.5 bg-primary/60 hover:bg-primary transition-colors" />
 
-        {/* File Preview Panel - 40% default */}
-        <Panel defaultSize={40} minSize={20} maxSize={60} className="relative p-4">
+        {/* File Preview Panel - 60% default */}
+        <Panel defaultSize={60} minSize={40} maxSize={80} className="relative p-4">
           <div className="h-full w-full rounded-xl overflow-hidden bg-background shadow-lg">
             <TabbedFilePreview
               openFiles={openFiles}
